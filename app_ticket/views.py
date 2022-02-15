b4cc07305e55a9d55deba25ab4ee4194764686b1
JonasFiechter <jonasrfiechter@gmail.com>
2022-02-15 17:02:24 -0300

    Added a floating menu to the home.html and ajusted some other configs

--- app_ticket/views.py
+++ app_ticket/views.py
@@ -17,4 +17,15 @@ def ticket_view_nuias_form(request):
         form.save()
         return redirect('url_home')
 
-    return render(request, 'app_ticket/ticket_form.html', data)
+    return render(request, 'app_ticket/ticket_nuias_form.html', data)
+
+
+def ticket_view_infra_form(request):
+    form = TicketForm(request.Post or None)
+    data = {'form': form}
+
+    if form.is_valid():
+        form.save()
+        print('working')
+
+    return render(request, 'app_ticket/ticket_infra_form.html', data)
