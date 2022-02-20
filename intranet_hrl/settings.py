c98973f25173a812be135ebcad4b8826da5c71f0
JonasFiechter <86444559+JonasFiechter@users.noreply.github.com>
2022-02-20 16:15:51 -0300

    Update settings.py

--- intranet_hrl/settings.py
+++ intranet_hrl/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s^ri9j@)7y7r&u8_*lz*^m7cu542g=^-af=j=^@k5%6h*3s2oi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
