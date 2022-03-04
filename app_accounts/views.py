4fc76af41ac45e064a5dc18720fe0ac9db4ac208
JonasFiechter <jonasrfiechter@gmail.com>
2022-03-04 18:06:18 -0300

    Some adjustments made

--- app_accounts/views.py
+++ app_accounts/views.py
@@ -25,23 +25,37 @@ def accounts_signup_view(request):
     last_name = request.POST.get('last_name')
     password = request.POST.get('password')
     password2 = request.POST.get('password2')
-    
+
+    print(f'{email}, {first_name}, {last_name}, {password}')
+
+    if len(first_name) == 0 or len(last_name) == 0:
+        messages.error(request, message='Digite o seu nome corretamente!')
+        return render(request, 'app_accounts/signup.html')
+
+    if len(email) == 0:
+        messages.error(request, message='Digite o seu email corretamente!')
+        return render(request, 'app_accounts/signup.html')
+
     if User.objects.filter(email=email).exists():
-        messages.error(request, message='Email already exists!')
+        messages.error(request, message='Este email já está cadastrado!')
+        return render(request, 'app_accounts/signup.html')
+
+    if len(password) < 6 or len(password2) < 6:
+        messages.error(request, message='A senha deve conter no mínimo 6 caracteres!')
         return render(request, 'app_accounts/signup.html')
 
     if password != password2:
-        messages.error(request, message="Passwords don't match!")
+        messages.error(request, message="As senhas devem ser iguais!")
         return render(request, 'app_accounts/signup.html')
 
     else:
         messages.success(request, 'Registrado com sucesso! Faça login.')
 
         user = User.objects.create_user(username=email, 
-                                    email=email, 
-                                    first_name=first_name,
-                                    last_name=last_name,
-                                    password=password)
+                                        email=email, 
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password)
 
         return redirect('url_login')
 
