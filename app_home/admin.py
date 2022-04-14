9b65566efe8f7189a74056fbf0122b961b0cc80a
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-14 15:35:26 -0300

    update
    New bug fixes and hrefs at home page

--- app_home/admin.py
+++ app_home/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from app_home.models import PhoneExtensions
+from app_home.models import PhoneExtensions, FunctionsBySector
 
 class PhoneExtensionsAdmin(admin.ModelAdmin):
     list_display = ['id', 'branch', 'sector', 'floor', 'description']
 
 
+class FunctionsBySectorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'branch', 'sector', 'description']
+
 admin.site.register(PhoneExtensions, PhoneExtensionsAdmin)
+admin.site.register(FunctionsBySector, FunctionsBySectorAdmin)
--- app_home/models.py
+++ app_home/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 from app_ticket.models import Sector
 
@@ -11,3 +12,13 @@ class PhoneExtensions(models.Model):
     sector = models.ForeignKey(Sector,verbose_name='Setor',  on_delete=models.DO_NOTHING)
     floor = models.IntegerField(verbose_name='Andar', null=True)
     description = models.TextField(verbose_name='Descrição', max_length=255, null=True)
+
+
+class FunctionsBySector(models.Model):
+    class Meta:
+        verbose_name_plural = 'Funções setoriais'
+    
+    branch = models.IntegerField(verbose_name='Ramal', null=True)
+    name = models.CharField(verbose_name='Nome da atividade', max_length=255, null=True)
+    description = models.TextField(verbose_name='Descrição', null=True)
+    sector = models.ForeignKey(Sector, verbose_name='Setor', on_delete=models.DO_NOTHING)
