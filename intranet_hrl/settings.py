1b3e806407a22bbd6b1b2bf006d567fdfad4c655
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-20 16:49:04 -0300

    important change
    Timezone in settings.py must be 'america/Sao_Paulo'

--- intranet_hrl/settings.py
+++ intranet_hrl/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import tzdata
 from pathlib import Path
 import os
 
@@ -116,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
