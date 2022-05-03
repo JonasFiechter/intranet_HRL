802cfbebd2fb9a653ae8f32070efe085d5217102
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-03 20:28:56 -0300

    update
    almost done

--- app_quality/static/app_quality/vendor/js/script.js
+++ app_quality/static/app_quality/vendor/js/script.js
@@ -1,3 +1,17 @@
 'use strict'
 
-console.log('working')
+let btn_radio = document.querySelectorAll('input[name="with-patient-option"]');
+console.log(btn_radio)
+const patient_fields_radio = function() {
+    console.log('clicked')
+}
+
+if (btn_radio) {
+    btn_radio.forEach((item) => {
+        item.addEventListener('change', function(event) {
+            var item = event.target.value;
+            console.log(item)
+            console.log('working')
+        })
+    })
+}
