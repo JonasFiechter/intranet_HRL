b84216dd84b777cafd807f1620f9790320b66d57
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-03 16:01:47 -0300

    update
    using date and time field on html input tag gives a better and easier way to post the data to the backend

--- app_quality/models.py
+++ app_quality/models.py
@@ -1,5 +1,3 @@
-from ssl import VerifyFlags
-from tabnanny import verbose
 from django.db import models
 from app_ticket.models import Sector
 
@@ -7,7 +5,7 @@ from app_ticket.models import Sector
 
 class Notification(models.Model):
     name = models.CharField(verbose_name='Nome do Notificante', max_length=255, blank=True)
-    date = models.DateTimeField(verbose_name='Data')
+    date = models.DateField(verbose_name='Data')
     phone = models.CharField(verbose_name='Telefone', max_length=255)
     function = models.CharField(verbose_name='Cargo/Função', max_length=255)
     sector = models.CharField(verbose_name='Setor', max_length=255)
