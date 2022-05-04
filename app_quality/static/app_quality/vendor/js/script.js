3b352137e3146ac8bd3f0d66b2e935e82f1bbfe0
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-04 16:33:10 -0300

    update
    notification forms front-end working

--- app_quality/static/app_quality/vendor/js/script.js
+++ app_quality/static/app_quality/vendor/js/script.js
@@ -1,8 +1,10 @@
 'use strict'
 
 let btn_radio = document.querySelectorAll('.yes-no');
-const btn = document.querySelector('.btn1');
 const patient_forms = document.querySelectorAll('.patient-form');
+
+let btn_radio_drug = document.querySelectorAll('.yes-no-drug');
+const drug_forms = document.querySelectorAll('.drug-form');
 
 
 if (btn_radio) {
@@ -16,8 +18,26 @@ if (btn_radio) {
                 } else if (item === 'no') {
                     for (const i of patient_forms) {
                         i.classList.add('hidden')
-                    }          
+                }          
             }
         })
     })
 }
+
+if (btn_radio_drug) {
+    btn_radio_drug.forEach((item) => {
+        item.addEventListener('change', function(event) {
+            var item = event.target.value;
+            if (item === 'yes') {
+                for (const i of drug_forms) {
+                    i.classList.remove('hidden');
+                    }
+                } else if (item === 'no') {
+                    for (const i of drug_forms) {
+                        i.classList.add('hidden')
+                }          
+            }
+        })
+    })
+}
+
