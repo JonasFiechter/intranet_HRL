e1beb78b81e0d5321217678608ed5527167ff94d
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-05 18:38:19 -0300

    update
    almost done edit mode on blog admin page

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -13,7 +14,11 @@ from .models import *
 @login_required(redirect_field_name='url_login')
 def blog_admin_view(request, action, post_id):
 
-    posts = Post.objects.order_by('-id')
+    #  Check if the group below is linked to the active user
+    if request.user.groups.filter(name='GROUP-QUALIDADE').exists():
+        is_valid = False
+        form = PostForm(request.POST, request.FILES)
+        posts = Post.objects.order_by('-id')
     
     #  In case of button delete is pressed and confirmed
     #  the confirmation step is written in a JavaScript file inside static folder
@@ -21,16 +26,12 @@ def blog_admin_view(request, action, post_id):
         try:
             post = Post.objects.get(id=post_id)
             post.delete()
-            print('success!')
-
+            return redirect('url_blog_admin', action='blank', post_id=0)
         except:
             print('error ocurred!')
 
-    #  Check if the group below is linked to the active user
-    if request.user.groups.filter(name='GROUP-ENGENHARIA').exists():
-        is_valid = False
-        form = PostForm(request.POST, request.FILES)
-
+    #  In case of comming for the first time or refreshing the page
+    elif action == 'blank':
         #  In case of form is valid
         if form.is_valid():
             form.save()
@@ -43,6 +44,14 @@ def blog_admin_view(request, action, post_id):
         return render(request, 'app_blog/blog_admin.html', {'form': form,
                                                             'is_valid': is_valid,
                                                             'posts': posts})
+    #  In case of edit button is pressed
+    if action == 'edit':
+        edit_mode = True
+        form = PostForm(request.POST, request.FILES, instance=post_id)
+        return render(request, 'app_blog/blog_admin.html', {'form': form,
+                                                    'is_valid': is_valid,
+                                                    'posts': posts,
+                                                    'edit_mode': edit_mode})
 
     else:
         messages.error(request, message='Você não tem permissão para acessar esta sessão!')
