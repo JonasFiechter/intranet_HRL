b121d6ee4b61eb33f46ce3811b89d61c75806ec1
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-17 16:32:31 -0300

    update
    files walker updated

--- intranet_hrl/utils/files_walker.py
+++ intranet_hrl/utils/files_walker.py
@@ -2,20 +2,16 @@ from ntpath import join
 import os
 
 def files_walker(root_dir, last_dir, next_dir, dirs=[], files=[], history={}):
-    print(f'\ntop of function - {root_dir} {last_dir} {next_dir}\n')
     on_root = False
     path = root_dir
 
     if ':' not in last_dir:
         history = {'last_dir': 'blank', 'next_dir': 'root'}
-        print(history)
     else:
         dirs = [d for d in last_dir.split(':')]
         next_dir_for_history = dirs.pop()
         last_dir_for_history = ':'.join(dirs)
-
         history = {'last_dir': last_dir_for_history, 'next_dir': next_dir_for_history}
-        print(history)
 
     if next_dir != 'root':
         path = last_dir.replace(':', '/') + '/' + next_dir
@@ -25,14 +21,11 @@ def files_walker(root_dir, last_dir, next_dir, dirs=[], files=[], history={}):
     if last_dir != 'blank' and next_dir != 'root':
         last_dir += ':' + next_dir
 
-
     dirs = [d for d in last_dir.split(':')]
-    print(f'history {history} | dirs {dirs}')
     
-    print(f'path before walk - {path}\n')
     for root, _dirs, files in os.walk(path):
         dirs = [d for d in _dirs]
-        files = [{'path': str(root[1:] + '/' + f), 'file': f} for f in files]
+        files = [{'path': str(root + '/' + f), 'file': f} for f in files]
         # print(f'files > {files} dirs > {dirs} root > {root}')
         break
 
