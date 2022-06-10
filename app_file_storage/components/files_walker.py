05ee3047e933e4c7c2ce9a9deb9c3a817d2e3f26
JonasFiechter <jonasrfiechter@gmail.com>
2022-06-10 11:25:05 -0300

    solved child from root folder issue

--- app_file_storage/components/files_walker.py
+++ app_file_storage/components/files_walker.py
@@ -1,6 +1,35 @@
 import os
 
 
+def files_walker(root_dir, path, dirs=[], files=[]):
+
+    # The way that this method works can be a little tricky but is much smaller than the older one
+    # The first check is determine if the history string will be populated based on the position of
+    # the path, because if path is one of the child of the root folder it will return to the root
+    # folder, and if it is empty (because is the root folder itself) it just goes empty
+
+    history = '/'.join(path.split('/')[:-1])
+    
+    # path_list = path.split('/')
+    # The content of the path splited items can be usefull to solve the problem with the children 
+    # of the root folder that was only True when inside the second level below the root, so this 
+    # if can solve this little issue  
+    if not path.split('/')[0] and path.split('/')[1]:
+        history = '/'
+
+    # print(f'history => {history} path_list => {path_list}')
+
+    # This loop breaks on the first walk inside the path given. And returns to lists with dicts
+    # one for every directory and other for each file inside the path.
+
+    for root, dirs, files in os.walk(root_dir + '/' + path):
+        dirs = [{'name': d, 'path': path + '/' + d} for d in dirs]
+        files = [{ 'file': f, 'path': str(root + '/' + f)} for f in files]
+        break
+
+    return dirs, files, history
+
+
 def files_walker_old(root_dir, last_dir, next_dir, dirs=[], files=[], history={}):
     on_root = False
     path = root_dir
@@ -33,23 +62,3 @@ def files_walker_old(root_dir, last_dir, next_dir, dirs=[], files=[], history={}
     return  dirs, files, last_dir, history
 
 
-def files_walker(root_dir, path, dirs=[], files=[]):
-
-    # The way that this method works can be a little tricky but is much smaller than the older one
-    # The first check is determine if the history string will be populated based on the position of
-    # the path, because if path is one of the child of the root folder it will return to the root
-    # folder, and if it is empty (because is the root folder itself) it just goes empty
-
-    history = '/'.join(path.split('/')[:-1])
-    if not history and path != '/':
-        history = '/'
-
-    # This loop breaks on the first walk inside the path given. And returns to lists with dicts
-    # one for every directory and other for each file inside the path.
-
-    for root, dirs, files in os.walk(root_dir + '/' + path):
-        dirs = [{'name': d, 'path': path + '/' + d} for d in dirs]
-        files = [{ 'file': f, 'path': str(root + '/' + f)} for f in files]
-        break
-
-    return dirs, files, history
